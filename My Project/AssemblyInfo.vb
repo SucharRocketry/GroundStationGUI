413199c282680e5b8ad6b480c3a1c09e77c8e018
Suchar <Suchar@DESKTOP-45E0H22>
2023-11-16 17:52:52 +0100

    Dodaj pliki projektów.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+' zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' powiązane z zestawem.
+
+' Sprawdź wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("GroundStationGUI")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GroundStationGUI")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Następujący identyfikator GUID jest identyfikatorem elementu typelib w przypadku udostępnienia tego projektu w modelu COM
+<Assembly: Guid("a844378b-c955-4040-9cd2-66d9c571c5bc")>
+
+' Informacje o wersji zestawu zawierają następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza
+'      Numer kompilacji
+'      Poprawka
+'
+' Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+' przy użyciu symbolu „*”, tak jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
